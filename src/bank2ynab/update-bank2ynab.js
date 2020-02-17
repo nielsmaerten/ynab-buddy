7e700ed94ccb9132773b9c29c4db8b7cea7760a6
Niels Maerten <niels@maerten.be>
2020-02-18 00:47:15 +0100

    allow overriding bank2ynab url

--- src/bank2ynab/update-bank2ynab.js
+++ src/bank2ynab/update-bank2ynab.js
@@ -1,19 +1,22 @@
 // bank2ynab: https://github.com/bank2ynab/bank2ynab/
 
+// If you want to use your own version of bank2ynab,
+// you can override the default url by calling: bank2ynab.update('https://my.custom.url')
+// Or, you can edit the following constant:
+const configUrl = "https://raw.githubusercontent.com/bank2ynab/bank2ynab/develop/bank2ynab.conf";
+
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
-
-const configUrl = "https://raw.githubusercontent.com/bank2ynab/bank2ynab/develop/bank2ynab.conf";
 const configFilePath = path.resolve("./src/bank2ynab/bank2ynab.conf");
 
 /**
  * Downloads and writes the current version of bank2ynab.conf to configFilePath
  */
-const update = () =>
+const update = (overrideUrl) =>
   new Promise(resolve => {
     console.log("Updating bank2ynab.conf ...");
-    https.get(configUrl).on("response", function(response) {
+    https.get(overrideUrl || configUrl).on("response", function(response) {
       let body = "";
       response.on("data", function(chunk) {
         body += chunk;
