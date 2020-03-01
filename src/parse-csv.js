609782776f39180ec068830f6c4e3088150923a4
Niels Maerten <niels@maerten.be>
2020-03-01 02:18:21 +0100

    add customConfig overrides

--- src/parse-csv.js
+++ src/parse-csv.js
@@ -4,14 +4,16 @@ const parsingTools = require("./parsing-tools");
  * Turn CSV content into YNAB compatible Transactions
  * @param {string} csvString A raw CSV string from a bank export file. Required
  * @param {string} filename The name of the bank export file. Required
+ * @param {any} customConfig Will be merged with detected config. Optional
  */
-const parseCsv = (csvString, filename) => {
+const parseCsv = (csvString, filename, customConfig) => {
   // Required parameters present?
   if (!csvString || !filename) return result("required_param_missing", filename);
 
   // Find a matching bank2ynab config
-  let config = parsingTools.detectConfig(csvString, filename);
-  if (!config) return result("no_matching_config", filename);
+  let config = parsingTools.detectConfig(csvString, filename) || {};
+  Object.assign(config, customConfig || {});
+  if (config === {}) return result("no_matching_config", filename);
 
   // Parse Transactions
   let transactions = parsingTools.parse(csvString, config);
--- src/parse-file.js
+++ src/parse-file.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const parse = require("./parse-csv");
 
-module.exports = filepath => {
+module.exports = (filepath, customConfig) => {
   let contents = fs.readFileSync(filepath).toString();
-  return parse(contents, filepath);
+  return parse(contents, filepath, customConfig);
 };
