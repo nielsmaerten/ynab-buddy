afcd527373576fe8303d8b861e210c8210fcfc16
Niels Maerten <niels@maerten.be>
2020-03-03 12:10:43 +0100

    update validateconfig to check to string dates

--- src/parsing-tools/validate-config.js
+++ src/parsing-tools/validate-config.js
@@ -7,31 +7,22 @@ const validate = (csvString, config) => {
   let transactions = parse(csvString, config, true);
   if (transactions.length === 0) return false;
 
-  let hasDate = checkProperty(transactions[0], "Date", Date);
-  let hasAmount = checkProperty(transactions[0], "Amount", "number");
-  let hasInflow = checkProperty(transactions[0], "Inflow", "number");
-  let hasOutflow = checkProperty(transactions[0], "Outflow", "number");
+  let hasDate = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/g.test(transactions[0].Date);
+  let hasAmount = checkNumber(transactions[0], "Amount");
+  let hasInflow = checkNumber(transactions[0], "Inflow");
+  let hasOutflow = checkNumber(transactions[0], "Outflow");
 
   let isValidTransaction = hasDate && (hasInflow || hasOutflow || hasAmount);
 
-  // FIXME: Some files still fall through the cracks,
-  // It might be useful to have a 'nuclear' option here,
-  // which will just try all configs and pick one that
-  // manages to deliver a valid transaction?
   return isValidTransaction;
 };
 
-const checkProperty = (transaction, propName, type) => {
+const checkNumber = (transaction, propName) => {
   let hasProperty = transaction.hasOwnProperty(propName);
   let property = transaction[propName];
+  let isTypeNumber = typeof property === "number";
 
-  let isCorrectType = false;
-  if (typeof type === "function") {
-    isCorrectType = property instanceof type;
-  } else {
-    isCorrectType = typeof property === type;
-  }
-  return hasProperty && isCorrectType;
+  return hasProperty && isTypeNumber;
 };
 
 module.exports = validate;
