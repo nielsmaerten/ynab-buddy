401b9c97191feb8ff6363348e9d48fe6834ef162
Niels Maerten <niels@maerten.be>
2020-03-03 19:35:44 +0100

    upload to ynab (#2)
    * install ynab and proxyquire
    
    * implement upload-result
    
    * quickfixes

--- src/upload-result.js
+++ src/upload-result.js
@@ -1,4 +1,67 @@
-module.exports = () => {
-  // TODO
-  return {};
+const ynab = require("ynab");
+
+module.exports = async (transactions, ynabToken, budgetId, accountId) => {
+  const api = new ynab.API(ynabToken);
+  const data = prepForYnab(transactions, accountId);
+  const apiResponse = await api.transactions.createTransactions(budgetId, data);
+
+  return apiResponse;
+};
+
+const prepForYnab = (transactions, account_id) => {
+  return {
+    transactions: transactions
+      
+      // Skip transactions that do not have a date
+      .filter(t => t.Date)
+
+      .map(t => {
+        return {
+          //category_id: "8d505bc0-efbe-4803-b8ef-0898213a1092",
+          //payee_id: null,
+          //cleared: ynab.SaveTransaction.ClearedEnum.Cleared,
+          //approved: true,
+          account_id,
+          date: t.Date,
+          amount: getAmount(t),
+          memo: t.Memo
+        };
+      })
+
+      // Add ids to prevent duplicate imports
+      .map(addImportId)
+  };
+};
+
+const count = {};
+/**
+ * From YNAB's API docs: the import_id is a string property that can be added to Transactions.
+ * It's used to match up Transactions that have been imported multiple times, or from different sources.
+ * An importId has the following format: `YNAB:[milliunit_amount]:[iso_date]:[occurrence]`
+ * Occurrence starts at 1, but if there's another transaction with the exact same amount on
+ * the same day, it would have an Occurrence of 2.
+ */
+const addImportId = t => {
+  // Build the first part of import_id (YNAB:date:amount)
+  let milliunit_amount = t.amount;
+  let iso_date = t.date;
+  let amount_date = `YNAB:${milliunit_amount}:${iso_date}`;
+
+  // If this import_id was not seen before, init it's count at 0
+  if (count[amount_date] === undefined) {
+    count[amount_date] = 0;
+  }
+
+  // Increment for this occurrence
+  count[amount_date]++;
+
+  // Set the full import_id (YNAB:date:amount:occurrence)
+  t.import_id = `${amount_date}:${count[amount_date]}`;
+  return t;
+};
+
+const getAmount = transaction => {
+  if (transaction.Inflow) return transaction.Inflow * 100;
+  if (transaction.Amount) return transaction.Amount * 100;
+  if (transaction.Outflow) return transaction.Outflow * 100 * -1;
 };
