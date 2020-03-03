d08a1e2435ef10e6c2ab982e0142afdfd54a9c58
Niels Maerten <niels@maerten.be>
2020-03-03 12:06:08 +0100

    start feature: save result

--- src/save-result.js
+++ src/save-result.js
@@ -1,4 +1,14 @@
-module.exports = () => {
-  // TODO
+const Papa = require("papaparse");
+// TODO
+module.exports = (parseResult, output) => {
+  let transactions = parseResult.transactions.filter(t => {
+    // Transactions that have no valid Date should be skipped
+    return !t.Date;
+  });
+
+  let csvString = Papa.unparse(transactions, {
+    header: true
+  });
+  debugger;
   return {};
 };
