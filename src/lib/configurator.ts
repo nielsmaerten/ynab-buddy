8f795087b3018279a92e9f59056492255760659a
Niels Maerten <niels@maerten.be>
2021-08-27 23:41:38 +0200

    Init jest

---
+++ src/lib/configurator.ts
@@ -0,0 +1,22 @@
+import { Configuration } from "../types";
+
+/**
+ * Reads the configuration file from disk and returns it as a Configuration object
+ * @returns A Configuration object with all settings required to run ynab-buddy
+ */
+export function getConfiguration(): Configuration | undefined {
+    // TODO: read from file or return empty if file is not found
+    return {
+        importFolder: {
+            path: '',
+            exists: false
+        },
+        bankFiles: [],
+        ynab: {
+            token: 'ABC123',
+            upload: true
+        },
+        parsers: [],
+        showConfigPrompt: undefined
+    }
+}
---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+export type Configuration = {
+  importFolder: {
+    path: string,
+    exists: boolean
+  };
+  parsers: Parser[];
+  bankFiles: BankFile[];
+  ynab: {
+    token: string;
+    upload: boolean;
+  };
+  showConfigPrompt?: boolean
+};
+
+export type BankFile = {
+    pattern: string;
+    parser: string;
+    name: string;
+}
+
+export type Parser = {
+  name: string;
+  columns: string[];
+  delimiter: string;
+  dateFormat: string;
+  footerRows: number;
+  headerRows: number;
+};
