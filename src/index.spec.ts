cf7138ef6ed478e5cf1ac1e306b36e3bbb299f12
Niels Maerten <niels@maerten.be>
2021-08-28 14:07:33 +0200

    update index tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,33 +1,49 @@
-const getConfiguration_mock = jest
-  .fn()
-  .mockReturnValue({ showConfigPrompt: true });
+let firstRun: any = "";
+let getConfiguration_mock: jest.Mock;
 const displayWelcomeMessage_mock = jest.fn();
 const exit_mock = jest.fn();
 
-// TODO: Next steps:
-/*
-These tests have to be implemented:
-- Define more mocks so that we can test all code currently in src/index.ts
-- Ensure all 3 test cases properly validate what they're describing, and make them green
-*/
 describe("index.ts", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     jest.mock("./lib/configurator", () => {
+      getConfiguration_mock = jest.fn().mockReturnValue({showConfigPrompt: firstRun});
       return {
         getConfiguration: getConfiguration_mock,
       };
     });
+    jest.mock("./lib/cli", () => {
+      return {
+        displayWelcomeMessage: displayWelcomeMessage_mock,
+      };
+    });
+    jest.mock("process", () => {
+      return {
+        exit: exit_mock,
+      };
+    });
   });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   it("gets configuration", () => {
     require("./index");
     expect(getConfiguration_mock).toHaveBeenCalled();
   });
 
-  it("displays welcome message according to firstRun", () => {
-    //expect(displayWelcomeMessage_mock).toHaveBeenCalled();
+  it("displays welcome message and exits if firstRun is true", () => {
+    firstRun = true;
+    jest.resetModules();
+    require("./index");
+    expect(displayWelcomeMessage_mock).toHaveBeenCalledWith(firstRun);
+    expect(exit_mock).toHaveBeenCalled();
   });
 
-  it("exits if firstRun is true", () => {
-    //expect(exit_mock).toHaveBeenCalled();
+  it("displays welcome message and does not exit if firstRun is false", () => {
+    firstRun = false;
+    jest.resetModules();
+    require("./index");
+    expect(displayWelcomeMessage_mock).toHaveBeenCalledWith(firstRun);
+    expect(exit_mock).not.toHaveBeenCalled();
   });
 });
